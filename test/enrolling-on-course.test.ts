6c081ab1e7c59956386a0a0664c9d1a62833a1e9
Cherif Bouchelaghem <cherif.bouchelaghem@gmail.com>
2024-08-06 09:37:48 -0400

    dependecies parametrization

--- test/enrolling-on-course.test.ts
+++ test/enrolling-on-course.test.ts
@@ -9,30 +9,40 @@ type Course = {
   learners: string[];
 }
 
-const courses: Course[] = []
+const courses: Map<string, Course> = new Map()
 
-const proposeCourse = (title: string, min: number, max: number) => {
+type CourseForTitle = (title: string) => Course | undefined
+
+type SaveCourse = (course: Course) => void
+
+const courseForTitle: CourseForTitle = (title: string) => courses.get(title)
+
+const saveCourse: SaveCourse = (course: Course) => courses.set(course.title, course)
+
+
+const proposeCourse = (saveCourse: SaveCourse, title: string, min: number, max: number) => {
   const course = { title: title, size: { min, max }, learners: [] }
-  courses.push(course)
+  saveCourse(course)
 }
 
-const enrol = (learner: string, title: string) => {
-  const course = courses.find(course => course.title === title)
+const enrol = (courseForTitle: CourseForTitle, saveCourse: SaveCourse, learner: string, title: string) => {
+  const course = courseForTitle(title)
   if (course) {
     course.learners.push(learner)
+    saveCourse(course)
   }
 }
 
-const isCourseViable = (title: string) => {
-  const course = courses.find(course => course.title === title)
+const isCourseViable = (courseForTitle: CourseForTitle, title: string) => {
+  const course = courseForTitle(title)
   if (course) {
     return course.learners.length >= course.size.min
   }
   return false;
 }
 
-const courseCanAcceptEnrollment = (title: string) => {
-  const course = courses.find(course => course.title === title)
+const courseCanAcceptEnrollment = (courseForTitle: CourseForTitle, title: string) => {
+  const course = courseForTitle(title)
   if (course) {
     return course.learners.length < course.size.max;
   }
@@ -43,58 +53,58 @@ describe("Enrolling on course", () => {
   test("Course does not get enough enrollments to be viable", () => {
     //Given "BDD for Beginners" was proposed with a class size of 2 to 3 people
     const title = "BDD for Beginners"
-    proposeCourse(title, 2, 3)
+    proposeCourse(saveCourse, title, 2, 3)
 
     //When only Alice enrols on this course
     const learner = "Alice"
 
-    enrol(learner, title)
+    enrol(courseForTitle, saveCourse, learner, title)
 
     //Then this course will not be viable
-    expect(isCourseViable(title)).toBeFalse();
+    expect(isCourseViable(courseForTitle, title)).toBeFalse();
 
   })
 
   test("Course gets enough enrollments to be viable", () => {
     //Given "BDD for Beginners" was proposed with a class size of 2 to 3 people
     const title = "BDD for Beginners"
-    proposeCourse(title, 2, 3)
+    proposeCourse(saveCourse, title, 2, 3)
 
     // Given Alice has already enrolled on this course
     const learnerAlice = "Alice"
-    enrol(learnerAlice, title)
+    enrol(courseForTitle, saveCourse, learnerAlice, title)
 
     // When Bob enrols on this course
     const learnerBob = "Bob"
-    enrol(learnerBob, title)
+    enrol(courseForTitle, saveCourse, learnerBob, title)
 
     //Then this course will be viable
-    const isViable = isCourseViable(title)
+    const isViable = isCourseViable(courseForTitle, title)
     expect(isViable).toBeTrue();
   })
 
   test("Enrollments are stopped when class size is reached", () => {
     //Given "BDD for Beginners" was proposed with a class size of 2 to 3 people
     const title = "BDD for Beginners"
-    proposeCourse(title, 2, 3)
+    proposeCourse(saveCourse, title, 2, 3)
 
     // Given Alice, Bob and Charlie have already enrolled on this course
     const learnerAlice = "Alice"
-    enrol(learnerAlice, title)
+    enrol(courseForTitle, saveCourse, learnerAlice, title)
 
     const learnerBob = "Bob"
-    enrol(learnerBob, title)
+    enrol(courseForTitle, saveCourse, learnerBob, title)
 
     const learnerCharlie = "Charlie"
-    enrol(learnerCharlie, title)
+    enrol(courseForTitle, saveCourse, learnerCharlie, title)
 
     // When Derek tries to enrol on this course
     const learnerDerek = "Derek"
     expect(() => {
-      if (!courseCanAcceptEnrollment(title)) {
+      if (!courseCanAcceptEnrollment(courseForTitle, title)) {
         throw new Error("Class is already at capacity")
       }
-      enrol(learnerDerek, title)
+      enrol(courseForTitle, saveCourse,learnerDerek, title)
     }).toThrow()
   })
 })
